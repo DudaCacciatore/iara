e5b09ae668ed5b2aa2a771965cc8f5b3f09f28f0
DudaCacciatore <158073263+DudaCacciatore@users.noreply.github.com>
2025-03-21 12:55:20 -0300

    Animando imagens do index

---
+++ animate/imagensIndex.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    document.querySelector('.img2').classList.add('show');
+}, 500);
+
+setTimeout(function() {
+    document.querySelector('.img3').classList.add('show');
+}, 1000);
+
+setTimeout(function() {
+    document.querySelector('.img4').classList.add('show');
+},1500);
